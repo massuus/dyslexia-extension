d6201be4aefea379bcc169cf4b682b3650251c07
i485097 <i485097@fhict.nl>
2025-05-24 15:07:25 +0200

    feat: Add common words list, enhance popup UI, and implement typography controls

--- background.js
+++ background.js
@@ -1,5 +1,4 @@
 chrome.runtime.onInstalled.addListener(() => {
-  console.log("[BG] Installed");
   chrome.contextMenus.create({
     id: "askPage",
     title: "Ask a question about this page...",
@@ -8,13 +7,10 @@ chrome.runtime.onInstalled.addListener(() => {
 });
 
 chrome.contextMenus.onClicked.addListener((info, tab) => {
-  console.log("[BG] Context menu clicked", info);
 
   chrome.tabs.sendMessage(tab.id, { type: "askPagePrompt" }, (res) => {
     if (chrome.runtime.lastError) {
       console.warn("[BG] SendMessage failed:", chrome.runtime.lastError.message);
-    } else {
-      console.log("[BG] Message sent");
     }
   });
 });
